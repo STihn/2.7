0fd03d9aebb26f6f3be02ea453489716864084a4
STihn <you.lucky@bk.ru>
2019-11-20 09:53:09 +0300

    4.6-release

---
+++ js/4.6.js
@@ -0,0 +1,70 @@
+
+
+
+// TASK 1
+let btn = document.getElementById('button');
+
+ function someFunc() {
+    const val = document.getElementById('firstName').value
+    if(val == '') {
+            alert('Вы забыли ввести Имя.');
+                return false;
+        }   
+    
+    let name = val
+    
+    document.getElementById('resultName').innerHTML = name
+    document.getElementById('firstName').value = ''
+}
+
+// TASK 2
+function Foo() {
+    let x = parseInt(prompt('Введите первое число'))
+    let y = parseInt(prompt('Введите второе число'))
+    if (!parseInt(x)){
+        alert('Вы ввели не число');
+    }
+    else if (!parseInt(y)) {
+        alert('Вы ввели не число');
+    }
+    if (x > y) {
+        alert( x );
+    }
+    if (x == y){
+        alert( x );
+    }
+    if (x < y) {
+        alert( y );
+    }
+}
+
+//  TASK 3
+
+function someFoo() {
+    let x = parseInt(prompt('Введите первое число'))
+    let y = parseInt(prompt('Введите второе число'))
+    if (!parseInt(x)){
+        alert('Вы ввели не число');
+    }
+    else if (!parseInt(y)) {
+        alert('Вы ввели не число');
+    }
+    if (x > y) {
+        alert( x );
+    }
+    if (Math.abs(x) == Math.abs(y)){
+        alert( x );
+    }
+    if (x < y) {
+        alert( y );
+    }
+}
+
+// TASK 4
+
+function someFoo (min, max) {
+
+   return Math.floor(Math.random() * (max - min) + min);
+
+}
+console.log(someFoo (1, 100));
